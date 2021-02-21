202871b9274f6f46623c9165efdfcd5178ff6b90
Huzaifa Zia <57328992+huzaifazia17@users.noreply.github.com>
2021-02-21 16:24:27 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -26,4 +26,49 @@ $(document).ready(function() { /* Jquery code must start like this*/
     $('.js--scroll-to-start').click(function(){
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 500)
     });
+
+    $(function() {
+        $('a[href*=#]:not{[href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target: $('[name=' + this.hash.slice(1) + ']');
+                if(target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animate__fadeIn');
+    }, {
+        offset: '50%;'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animate__fadeInUp');
+    }, {
+        offset: '50%;'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__fadeIn');
+    }, {
+        offset: '50%;'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animate__pulse');
+    }, {
+        offset: '50%;'
+    });
+
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon= $('js--nav-icon i');
+        nav.slideToggle(200);
+    });
 });
